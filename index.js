48519d165bba1b771508bf5cc4e9a031538ab36b
Zahurul Islam <zahurulislam803@gmail.com>
2023-08-06 10:53:49 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,96 @@
+// var totalMoney = 26000;
+// if(totalMoney > 80000){
+//   console.log('I wil buy a car');
+// }
+// else if(totalMoney > 60000){
+//   console.log('I will buy a gaming laptop');
+// }
+// else if(totalMoney > 40000){
+//   console.log('I will buy a Lenovo Yoga');
+// }
+// else if(totalMoney > 20000){
+//   console.log('I will buy an used laptop');
+// }
+// else{
+//   console.log('I will use mobile phone');
+// }
+
+
+// for(i = 38; i <86; i++){
+//   if(i > 60){
+//     break;
+//   }
+//   console.log(i);
+// }
+
+
+// var numbers = [255,240,451,120,700];
+// var i = 0;
+// while(i < numbers.length){
+//   var number = numbers[i];
+// if(number < 200){
+//   continue;
+// }
+// console.log(number);
+// i++;
+// }
+
+// var marks = [ 13, 15, 14, 20, 18];
+// for(var i=0; i<marks.length; i++){
+//  if(marks[i] >=15){
+//   continue;
+//  }
+//  console.log(marks[i]);
+// };
+
+
+
+// var i = 0;
+// for (i = 0; i < 5; i++) {}
+// console.log(i);
+
+
+// var allMobilePhones = ['symphony-D54', 'symphony-D55', 'symphony-D54i'];
+// var i =0;
+// while(i < allMobilePhones.length){
+//   var phones= allMobilePhones[i];
+//   console.log(allMobilePhones[i]);
+//   i++;
+// }
+
+
+// var allMobilePhones = ['symphony-D54', 'symphony-D55', 'symphony-D54i'];
+// var i =0;
+// while(i < allMobilePhones.length){
+//   console.log(allMobilePhones[i]);
+//   i++;
+// }
+
+
+// var names = ['samia', 'soniya', 'mahim', 'rassell', 'akram'];
+// var i = 0;
+// while(i < names.length){
+// var name = names[i];
+// i++;
+// if(name.length == 5){
+//   console.log('hi bribal');
+// }
+// }
+
+
+// var age = 45;
+// var hasLicense = false;
+
+// if(age >=18){
+//   if(hasLicense == true){
+//     console.log('you are adult with license');
+//   }
+//   else{
+//     console.log('you are adult but not license');
+//   }
+// }
+// else{
+//   console.log('You are not adult');
+// }
+
+// age >=18 ? hasLicense == true ? console.log('you are adult with license') : console.log('you are adult but not license') : console.log('You are not adult');
